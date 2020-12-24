6a0b97882a4b12cda3a62493d47421cd19f53212
anastasiia-kolieva <nastya1110@gmail.com>
2020-12-24 20:57:10 +0200

    made refactoring code of Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,41 +1,67 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    // слушатель событий на window, для закрытия модалки при нажатии на Esc
-    window.addEventListener('keydown', this.handelKeyDown);
-  }
+export default function Modal({ onClose, children }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handelKeyDown);
+    return window.removeEventListener('keydown', handelKeyDown);
+  }, []);
 
-  //   при размонтировании снимать слушатель.Почистила
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handelKeyDown);
-  }
-
-  handelKeyDown = event => {
+  const handelKeyDown = event => {
     if (event.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  //   закрытие модалки при клике на серый фон
-  handelOverlayClick = event => {
+  const handelOverlayClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={s.Overlay} onClick={this.handelOverlayClick}>
-        <div className={s.Modal}>{this.props.children}</div>
-      </div>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <div className={s.Overlay} onClick={handelOverlayClick}>
+      <div className={s.Modal}>{children}</div>
+    </div>,
+    modalRoot,
+  );
 }
 
-export default Modal;
+// class Modal extends Component {
+//   componentDidMount() {
+//     // слушатель событий на window, для закрытия модалки при нажатии на Esc
+//     window.addEventListener('keydown', this.handelKeyDown);
+//   }
+
+//   //   при размонтировании снимать слушатель.Почистила
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handelKeyDown);
+//   }
+
+//   handelKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   //   закрытие модалки при клике на серый фон
+//   handelOverlayClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={s.Overlay} onClick={this.handelOverlayClick}>
+//         <div className={s.Modal}>{this.props.children}</div>
+//       </div>,
+//       modalRoot,
+//     );
+//   }
+// }
+
+// export default Modal;
